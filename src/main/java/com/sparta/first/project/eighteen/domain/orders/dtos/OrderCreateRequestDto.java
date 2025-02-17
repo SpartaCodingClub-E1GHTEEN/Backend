c3a93bed634efcd53beb9f0fc44d909212f01bea
kade <bhpark93@gmail.com>
2025-02-17 11:03:04 +0900

    Rename: dto 이름 수정
    OrderRequestDto -> OrderCreateRequestDto

--- src/main/java/com/sparta/first/project/eighteen/domain/orders/dtos/OrderCreateRequestDto.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/orders/dtos/OrderCreateRequestDto.java
@@ -2,12 +2,13 @@ package com.sparta.first.project.eighteen.domain.orders.dtos;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class OrderRequestDto {
+public class OrderCreateRequestDto {
 	private String storeId;
 	private Boolean isStoreOrder;
 	private String noteToStore;
@@ -15,7 +16,7 @@ public class OrderRequestDto {
 	private List<OrderDetailsRequestDto> orderDetails;
 
 	//TODO: DELETE MOCKDATA
-	public OrderRequestDto(String id) {
+	public OrderCreateRequestDto(String id) {
 		this.storeId = "1";
 		this.isStoreOrder = false;
 		this.noteToStore = "맛있게 만들어주세요";
