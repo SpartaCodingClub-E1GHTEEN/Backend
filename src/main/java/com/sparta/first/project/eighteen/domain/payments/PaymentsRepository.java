e7a4ed8249e6a032612ca58bf3c0691c3d07d240
kade <bhpark93@gmail.com>
2025-02-17 19:57:09 +0900

    Feat: Payments Entity,Repository
    결제 Entity 구현
    결제 Repository 구현
    
    Github issue #39

---
+++ src/main/java/com/sparta/first/project/eighteen/domain/payments/PaymentsRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.first.project.eighteen.domain.payments;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.first.project.eighteen.model.payments.Payments;
+
+public interface PaymentsRepository extends JpaRepository<Payments, UUID> {
+}
