c0a2eeaa9c89a9adb3461d0fce890450cf8a3852
Dominic.Yoon <808dominico@naver.com>
2025-02-17 11:46:44 +0900

    refactor: endpoint를 단수에서 복수로 수정, RequestParam 사용하지 않고, ModelAttribute 사용으로 수정, updateFood에 PathVariable 추가

--- src/main/java/com/sparta/first/project/eighteen/domain/foods/FoodsController.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/foods/FoodsController.java
@@ -3,12 +3,12 @@ package com.sparta.first.project.eighteen.domain.foods;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.first.project.eighteen.common.dto.ApiResponse;
@@ -22,38 +22,35 @@ import com.sparta.first.project.eighteen.domain.foods.dtos.FoodSearchRequestDto;
 public class FoodsController {
 	private FoodsService foodsService;
 
-	@PostMapping("/food")
+	@PostMapping("/foods")
 	public ResponseEntity<ApiResponse<FoodResponseDto>> createFood(@RequestBody FoodCreateRequestDto requestDto) {
 
 		return null;
 	}
 
-	@GetMapping("/store/{storeId}/food")
+	@GetMapping("/stores/{storeId}/foods")
 	public ResponseEntity<ApiResponse<FoodGetResponseDto>> searchFood(@PathVariable String storeId,
-		@RequestParam(required = false) String keyword, @RequestParam(required = false) String filter,
-		@RequestParam(required = false) String sort, @RequestParam(defaultValue = "1") int page,
-		@RequestParam(defaultValue = "10") int limit) {
+		@ModelAttribute FoodSearchRequestDto requestDto) {
 
-		// DTO로 변환하여 서비스에 전달
-		FoodSearchRequestDto searchDto = new FoodSearchRequestDto(keyword, filter, sort, page, limit);
 		FoodGetResponseDto responseDto = null;
 
 		return null;
 	}
 
-	@GetMapping("/food/{foodId}")
+	@GetMapping("/foods/{foodId}")
 	public ResponseEntity<ApiResponse<FoodGetResponseDto>> getFood(@PathVariable String foodId) {
 
 		return null;
 	}
 
-	@PutMapping("/food/")
-	public ResponseEntity<ApiResponse<FoodResponseDto>> updateFood(@RequestBody FoodCreateRequestDto requestDto) {
+	@PutMapping("/foods/{foodId}")
+	public ResponseEntity<ApiResponse<FoodResponseDto>> updateFood(@PathVariable String foodId,
+		@RequestBody FoodCreateRequestDto requestDto) {
 
 		return null;
 	}
 
-	@DeleteMapping("/food/{foodId}")
+	@DeleteMapping("/foods/{foodId}")
 	public ResponseEntity<ApiResponse<Void>> deleteFood(@PathVariable String foodId) {
 
 		return null;
