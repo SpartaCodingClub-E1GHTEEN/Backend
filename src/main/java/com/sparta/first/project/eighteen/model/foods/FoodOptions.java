39d5f17267235b5db153dbd655fab8be88e98189
dominic.yoon <142984862+dominic-yoon@users.noreply.github.com>
2025-02-17 15:09:56 +0900

    Feat/30 (#37)
    * refactor: 컨트롤러에서 updateFood 파라미터 타입 수정 (FoodCreateRequestDto -> FoodUpdateRequestDto)
    
    * feat: Foods Entity 생성
    
    * feat: FoodOptions Entity 생성
    
    * feat: FoodRepository 생성

--- src/main/java/com/sparta/first/project/eighteen/model/foods/FoodOptions.java
+++ src/main/java/com/sparta/first/project/eighteen/model/foods/FoodOptions.java
@@ -1,4 +1,38 @@
 package com.sparta.first.project.eighteen.model.foods;
 
-public class FoodOptions {
+import java.util.UUID;
+
+import com.sparta.first.project.eighteen.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "p_food_option")
+public class FoodOptions extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column
+	private UUID foodId;
+
+	@Column
+	private String optionName;
+
+	@Column
+	private int optionPrice;
 }
--- src/main/java/com/sparta/first/project/eighteen/model/foods/Foods.java
+++ src/main/java/com/sparta/first/project/eighteen/model/foods/Foods.java
@@ -1,4 +1,59 @@
 package com.sparta.first.project.eighteen.model.foods;
 
-public class Foods {
+import java.util.UUID;
+
+import com.sparta.first.project.eighteen.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "p_foods")
+public class Foods extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column
+	private UUID storeId;
+
+	@Column
+	private String foodName;
+
+	@Column
+	private String foodDesc;
+
+	@Column
+	private int foodPrice;
+
+	@Column
+	private String foodCategory;
+
+	@Column
+	private String foodImageUrl;
+
+	@Column
+	private String foodStatus;
+
+	@Column
+	private boolean isRecommended;
+
+	@Column
+	private int foodReviewCount;
+
+	@Column
+	private int foodOrderCount;
 }
