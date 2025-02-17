e460baa2054773646e4cf397ef9868188aebedcc
azuressu <martina0130@naver.com>
2025-02-17 14:18:39 +0900

    Refactor : JPARepo 수정

--- src/main/java/com/sparta/first/project/eighteen/domain/stores/StoreRepository.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/stores/StoreRepository.java
@@ -1,8 +1,10 @@
 package com.sparta.first.project.eighteen.domain.stores;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sparta.first.project.eighteen.model.stores.Store;
 
-public interface StoreRepository extends JpaRepository<Store, String> {
+public interface StoreRepository extends JpaRepository<Store, UUID> {
 }
