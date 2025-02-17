f960a3fa7b895950fe4fc7aaec726205e6f2b34e
Dominic.Yoon <808dominico@naver.com>
2025-02-17 14:21:42 +0900

    feat: FoodRepository 생성

--- src/main/java/com/sparta/first/project/eighteen/domain/foods/FoodsRepository.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/foods/FoodsRepository.java
@@ -1,4 +1,10 @@
 package com.sparta.first.project.eighteen.domain.foods;
 
-public class FoodsRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sparta.first.project.eighteen.model.foods.Foods;
+
+public interface FoodsRepository extends JpaRepository<Foods, UUID> {
 }
