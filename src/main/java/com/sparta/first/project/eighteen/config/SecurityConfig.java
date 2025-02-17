01b058521e8633b78b511aa83f4548e0231ba8d7
cnxw4570123 <cnxw4570123@gmail.com>
2025-02-17 23:12:07 +0900

    Fix : h2-console에 접근할 수 없었던 문제 수정
    - 페이지 렌더링 여부를 결정하는 X-FRAME-OPTIONS가 deny로 되어있던 문제 확인 후 같은 도메인에 대해서는 허용하도록 수정

--- src/main/java/com/sparta/first/project/eighteen/config/SecurityConfig.java
+++ src/main/java/com/sparta/first/project/eighteen/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -63,10 +64,14 @@ public class SecurityConfig {
 		// 요청 필터링
 		http
 			.authorizeHttpRequests((auth) -> auth
-					.requestMatchers("/api/v1/auth/**").permitAll()  // swagger나 rest docs 넣는다면 추가
-					.anyRequest().permitAll()
-				// .anyRequest().authenticated() // 모든 경로 JWT 인증 필요
+				.requestMatchers("/api/v1/auth/**", "/h2-console/**").permitAll()  // swagger나 rest docs 넣는다면 추가
+				.anyRequest().authenticated() // 모든 경로 JWT 인증 필요
 			);
+
+		// X-FRAME-OPTIONS
+		http.headers(headers -> headers
+			.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+		);
 
 		return http.build();
 	}
