98793a42cfe04c47aa77e24773e500d981d2a7bb
ssu <74248726+azuressu@users.noreply.github.com>
2025-02-18 11:19:06 +0900

    Feat/42 Store, Review Dto 메서드 추가 (#49)
    * feat : review request에 toEntity 메서드 생성
    
    * feat : store request에 toEntity 메서드 생성
    
    * feat : store response에 fromEntity 메서드 생성
    
    * feat : review response에 fromEntity 메서드 생성
    
    * refactor : static method로 변경

--- src/main/java/com/sparta/first/project/eighteen/domain/stores/dtos/StoreListResponseDto.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/stores/dtos/StoreListResponseDto.java
@@ -3,6 +3,17 @@ package com.sparta.first.project.eighteen.domain.stores.dtos;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sparta.first.project.eighteen.model.stores.Stores;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreListResponseDto {
 
 	// 식당 ID
@@ -17,17 +28,29 @@ public class StoreListResponseDto {
 	// 식당 평점
 	private double storeRating;
 
-	// 식당에 판매하는 음식 리스트 -> FoodResponseDto 리스트로 변환
-	List<?> foodList;
+	// 식당 이미지
+	private String storeImgUrl;
+
+	// 식당 배달팁
+	private int storeDeliveryPrice;
 
 	// 테스트를 위한 임시 생성자
-	public StoreListResponseDto (StoreRequestDto storeRequestDto) {
+	public StoreListResponseDto(StoreRequestDto storeRequestDto) {
 		this.id = "1";
 		this.storeName = "한식당";
 		this.storeRegion = "서울 광화문";
 		this.storeRating = 4.3;
-		// FoodResponseDto 리스트로 변환
-		this.foodList = new ArrayList<>();
+	}
+
+	public static StoreListResponseDto fromEntity(Stores store) {
+		return StoreListResponseDto.builder()
+			.id(store.getId().toString())
+			.storeName(store.getStoreName())
+			.storeImgUrl(store.getStoreImgUrl())
+			.storeRegion(store.getStoreRegion())
+			.storeRating(store.getStoreRating())
+			.storeDeliveryPrice(store.getStoreDeliveryPrice())
+			.build();
 	}
 
 }
