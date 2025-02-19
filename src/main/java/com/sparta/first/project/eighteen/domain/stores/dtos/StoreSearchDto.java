749426093e097e684dbe700ae66fb4863793d447
azuressu <martina0130@naver.com>
2025-02-19 10:18:58 +0900

    refactor : searchDto에 필드 추가

--- src/main/java/com/sparta/first/project/eighteen/domain/stores/dtos/StoreSearchDto.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/stores/dtos/StoreSearchDto.java
@@ -2,11 +2,17 @@ package com.sparta.first.project.eighteen.domain.stores.dtos;
 
 import org.springframework.data.domain.Pageable;
 
+import com.sparta.first.project.eighteen.model.stores.StoreCategory;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoreSearchDto {
 
 	// 식당명
@@ -16,9 +22,12 @@ public class StoreSearchDto {
 	private String storeRegion;
 
 	// 식당 카테고리
-	private String storeCategory;
+	private StoreCategory storeCategory;
 
-	// pageable
-	private Pageable pageable;
+	// 식당 최소 배달팁
+	private int minDeliveryPrice;
+
+	// 식당 최대 배달팁
+	private int maxDeliveryPrice;
 
 }
