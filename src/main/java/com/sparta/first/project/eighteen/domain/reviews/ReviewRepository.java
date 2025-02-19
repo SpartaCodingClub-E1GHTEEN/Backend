5d5516aeeafcaa4eade5a03b1bc1aa47a7c8af27
azuressu <martina0130@naver.com>
2025-02-19 10:17:57 +0900

    feat : repo 메서드 추가

--- src/main/java/com/sparta/first/project/eighteen/domain/reviews/ReviewRepository.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/reviews/ReviewRepository.java
@@ -1,10 +1,15 @@
 package com.sparta.first.project.eighteen.domain.reviews;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sparta.first.project.eighteen.model.reviews.Reviews;
+import com.sparta.first.project.eighteen.model.stores.Stores;
 
 public interface ReviewRepository extends JpaRepository<Reviews, UUID> {
+	int countByStoreId(Stores storeId);
+
+	List<Integer> findReviewRatingByStoreId(Stores storeId);
 }
