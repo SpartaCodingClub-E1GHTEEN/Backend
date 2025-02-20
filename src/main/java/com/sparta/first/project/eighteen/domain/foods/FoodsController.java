a6f572ce52d5e1fc81446cdfa8ffd1bfbfa128df
Dominic.Yoon <808dominico@naver.com>
2025-02-21 01:20:38 +0900

    refactor: 각 API 마다 USER_ROLE 부여(예정)

--- src/main/java/com/sparta/first/project/eighteen/domain/foods/FoodsController.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/foods/FoodsController.java
@@ -3,6 +3,7 @@ package com.sparta.first.project.eighteen.domain.foods;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -22,13 +23,17 @@ import com.sparta.first.project.eighteen.domain.foods.dtos.FoodSingleResponseDto
 import com.sparta.first.project.eighteen.domain.foods.dtos.FoodUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j(topic = "FoodsController")
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class FoodsController {
 	private final FoodsService foodsService;
 
+	// USER_ROLE -> OWNER
+	@PreAuthorize("hasRole('MASTER') or hasRole('MANAGER') or hasRole('OWNER')")
 	@PostMapping("/foods")
 	public ResponseEntity<ApiResponse<FoodResponseDto>> createFood(@RequestBody FoodCreateRequestDto requestDto) {
 
@@ -37,6 +42,7 @@ public class FoodsController {
 		return ResponseEntity.ok(ApiResponse.ok("메뉴가 성공적으로 생성되었습니다.", responseDto));
 	}
 
+	// USER_ROLE -> ANYONE
 	@GetMapping("/stores/{storeId}/foods")
 	public ResponseEntity<ApiResponse<FoodGetResponseDto>> searchFood(@PathVariable UUID storeId,
 		@ModelAttribute FoodSearchRequestDto requestDto) {
@@ -46,6 +52,7 @@ public class FoodsController {
 		return ResponseEntity.ok(ApiResponse.ok("메뉴 검색 결과", responseDto));
 	}
 
+	// USER_ROLE -> ANYONE
 	@GetMapping("/foods/{foodId}")
 	public ResponseEntity<ApiResponse<FoodSingleResponseDto>> getFood(@PathVariable UUID foodId) {
 
@@ -54,6 +61,7 @@ public class FoodsController {
 		return ResponseEntity.ok(ApiResponse.ok("메뉴를 성공적으로 조회했습니다.", responseDto));
 	}
 
+	// USER_ROLE -> OWNER
 	@PutMapping("/foods/{foodId}")
 	public ResponseEntity<ApiResponse<FoodResponseDto>> updateFood(@PathVariable String foodId,
 		@RequestBody FoodUpdateRequestDto requestDto) {
@@ -61,6 +69,7 @@ public class FoodsController {
 		return null;
 	}
 
+	// USER_ROLE -> OWNER
 	@DeleteMapping("/foods/{foodId}")
 	public ResponseEntity<ApiResponse<Void>> deleteFood(@PathVariable UUID foodId) {
 
