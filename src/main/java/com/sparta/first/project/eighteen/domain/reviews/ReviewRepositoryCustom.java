5e9d59082df6bd77fa5c505ba98db85d8b55f7ff
azuressu <martina0130@naver.com>
2025-02-20 10:28:10 +0900

    refactor : review custom repo 수정

--- src/main/java/com/sparta/first/project/eighteen/domain/reviews/ReviewRepositoryCustom.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/reviews/ReviewRepositoryCustom.java
@@ -1,6 +1,5 @@
 package com.sparta.first.project.eighteen.domain.reviews;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.domain.Page;
@@ -11,7 +10,6 @@ import com.sparta.first.project.eighteen.domain.reviews.dtos.ReviewSearchDto;
 import com.sparta.first.project.eighteen.model.users.Role;
 
 public interface ReviewRepositoryCustom {
-	Page<ReviewResponseDto> searchReviews(ReviewSearchDto searchDto, Pageable pageable, Role role);
+	Page<ReviewResponseDto> searchReviews(ReviewSearchDto searchDto, Pageable pageable, Role role, UUID storeId);
 	ReviewResponseDto getOneReviewToDto(UUID reviewId);
-	List<ReviewResponseDto> findAllByStore(UUID storeId, Pageable pageable);
 }
