fae286d5e2673366fad51970b12dce85641c03ba
kade <bhpark93@gmail.com>
2025-02-21 03:15:00 +0900

    Feat: Order RequestDto Validation 추가
    - 주문 각 RequestDto에 유효성 검사 추가
    
    Resolves : Github Issue #61

--- src/main/java/com/sparta/first/project/eighteen/domain/orders/dtos/OrderCreateRequestDto.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/orders/dtos/OrderCreateRequestDto.java
@@ -3,11 +3,16 @@ package com.sparta.first.project.eighteen.domain.orders.dtos;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.sparta.first.project.eighteen.common.constants.Constant;
 import com.sparta.first.project.eighteen.model.orders.OrderStatus;
 import com.sparta.first.project.eighteen.model.orders.Orders;
 import com.sparta.first.project.eighteen.model.stores.Stores;
 import com.sparta.first.project.eighteen.model.users.Users;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,11 +21,23 @@ import lombok.Setter;
 @Getter
 @Setter
 public class OrderCreateRequestDto {
+
+	@NotBlank
 	private String storeId;
+
+	@NotNull
 	private Boolean isStoreOrder;
+
+	@Size(max = 50, message = Constant.ErrorMessage.NOTE_IS_TOO_LONG)
 	private String noteToStore;
+
+	@Size(max = 50, message = Constant.ErrorMessage.NOTE_IS_TOO_LONG)
 	private String noteToDelivery;
+
+	@Min(100)
 	private int totalPrice;
+
+	@NotNull
 	private List<OrderDetailsRequestDto> orderDetails;
 
 	public Orders toEntity(Stores store, Users user) {
--- src/main/java/com/sparta/first/project/eighteen/domain/orders/dtos/OrderDetailsRequestDto.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/orders/dtos/OrderDetailsRequestDto.java
@@ -4,14 +4,21 @@ import com.sparta.first.project.eighteen.model.foods.Foods;
 import com.sparta.first.project.eighteen.model.orders.OrderDetails;
 import com.sparta.first.project.eighteen.model.orders.Orders;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class OrderDetailsRequestDto {
+
+	@NotBlank
 	private String productId;
+
 	private String[] optionIds;
+
+	@Min(1)
 	private int amount;
 
 	public OrderDetails toEntity(Orders order, Foods food, int amount) {
