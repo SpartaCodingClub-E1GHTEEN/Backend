e55adc92c7163a669629a7470621d69467c39c1d
unknownKade <81609370+unknownKade@users.noreply.github.com>
2025-02-21 12:51:08 +0900

    Feat/69 order delete and status update (#72)
    * Feat: Order 삭제 및 상태변경 Controller 구현
    
    - Order 삭제 controller 구현
    - Order 상태 변경 controller 구현
    - Order Controller 주석 추가
    
    Github issue #69
    
    * Feat: Order 삭제 및 상태변경 Service 구현
    
    - Order 삭제 Service 구현
    - Order 상태 변경 Service 구현
    
    Github issue #69
    
    * Feat: 각 Api 접근 가능 Role 부여
    
    - 주문 Api에 각각 접근 가능한 Role Preauthorize
    
    * Feat: 주문 상태변경 상태 파라미터 받는 방법 변경
    
    주문 검색 요청 객체 요효성 검사 추가
    주문 상태변경 상태 파라미터 받는 방법 변경 PathVariable -> RequestParam

--- src/main/java/com/sparta/first/project/eighteen/domain/orders/OrderController.java
+++ src/main/java/com/sparta/first/project/eighteen/domain/orders/OrderController.java
@@ -2,6 +2,7 @@ package com.sparta.first.project.eighteen.domain.orders;
 
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sparta.first.project.eighteen.common.dto.ApiResponse;
@@ -19,6 +21,7 @@ import com.sparta.first.project.eighteen.domain.orders.dtos.OrderCreateRequestDt
 import com.sparta.first.project.eighteen.domain.orders.dtos.OrderResponseDto;
 import com.sparta.first.project.eighteen.domain.orders.dtos.OrderSearchRequestDto;
 import com.sparta.first.project.eighteen.domain.orders.dtos.OrderUpdateRequestDto;
+import com.sparta.first.project.eighteen.model.orders.OrderStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -29,6 +32,14 @@ public class OrderController {
 
 	private final OrderService orderService;
 
+	/**
+	 * 주문 생성
+	 *
+	 * @param requestDto : 주문 정보
+	 * @param user : 로그인한 사용자
+	 * @return  :
+	 */
+	@PreAuthorize("hasAnyRole('OWNER','CUSTOMER')")
 	@PostMapping
 	public ResponseEntity<ApiResponse<Void>> createOrder(@RequestBody OrderCreateRequestDto requestDto,
 		@AuthenticationPrincipal UserDetailsImpl user) {
@@ -36,12 +47,25 @@ public class OrderController {
 		return ResponseEntity.ok(ApiResponse.ok("주문이 완료되었습니다.", null));
 	}
 
+	/**
+	 * 주문 조회
+	 *
+	 * @param id : 조회할 주문 ID
+	 * @return requestDto : 주문 내용
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<ApiResponse<OrderResponseDto>> readOrder(@PathVariable String id) {
 		OrderResponseDto responseDto = orderService.readOrder(id);
 		return ResponseEntity.ok(ApiResponse.ok("주문을 조회했습니다.", responseDto));
 	}
 
+	/**
+	 * 주문 목록 조회
+	 *
+	 * @param requestDto : 주문 필터, 정렬, 페이징 정보
+	 * @return  : 주문 정보
+	 */
+	@PreAuthorize("hasAnyRole('MASTER','MANAGER','OWNER')")
 	@GetMapping
 	public ResponseEntity<ApiResponse<PagedModel<OrderResponseDto>>> searchOrder(
 		@ModelAttribute OrderSearchRequestDto requestDto) {
@@ -49,6 +73,14 @@ public class OrderController {
 		return ResponseEntity.ok(ApiResponse.ok("주문 목록을 조회했습니다", responseDto));
 	}
 
+	/**
+	 * 주문 수정
+	 *
+	 * @param id : 조회할 주문 ID
+	 * @param requestDto : 주문 수정 내용
+	 * @return  : 주문 정보
+	 */
+	@PreAuthorize("hasRole('MANAGER')")
 	@PatchMapping("/{id}")
 	public ResponseEntity<ApiResponse<OrderResponseDto>> updateOrder(@PathVariable String id,
 		@RequestBody OrderUpdateRequestDto requestDto) {
@@ -56,9 +88,45 @@ public class OrderController {
 		return ResponseEntity.ok(ApiResponse.ok("주문을 수정했습니다.", responseDto));
 	}
 
-	@DeleteMapping("/{id}")
+	/**
+	 * 주문 취소
+	 *
+	 * @param id : 조회할 주문 ID
+	 * @return  : 주문 정보
+	 */
+	@PreAuthorize("hasAnyRole('MANAGER','OWNER', 'CUSTOMER')")
+	@DeleteMapping("/{id}/cancel")
 	public ResponseEntity<ApiResponse<OrderResponseDto>> cancelOrder(@PathVariable String id) {
 		OrderResponseDto responseDto = orderService.cancelOrder(id);
 		return ResponseEntity.ok(ApiResponse.ok("주문을 취소했습니다.", responseDto));
+	}
+
+	/**
+	 * 주문 삭제(MASTER)
+	 *
+	 * @param id : 조회할 주문 ID
+	 * @return  :
+	 */
+	@PreAuthorize("hasRole('MASTER')")
+	@DeleteMapping("/{id}")
+	public ResponseEntity<ApiResponse<Void>> deleteOrder(@PathVariable String id,
+		@AuthenticationPrincipal UserDetailsImpl user) {
+		orderService.deleteOrder(id, user.getUserUUID().toString());
+		return ResponseEntity.ok(ApiResponse.ok("주문을 삭제했습니다.", null));
+	}
+
+	/**
+	 * 주문 상태변경 (OWNER/RIDER)
+	 *
+	 * @param id : 조회할 주문 ID
+	 * @param status : 주문 변경 상태
+	 * @return  : 주문 정보
+	 */
+	@PreAuthorize("hasAnyRole('OWNER','RIDER')")
+	@PatchMapping("/{id}")
+	public ResponseEntity<ApiResponse<OrderResponseDto>> updateOrderStatus(@PathVariable String id,
+		@RequestParam OrderStatus status) {
+		OrderResponseDto responseDto = orderService.updateOrderStatus(id, status);
+		return ResponseEntity.ok(ApiResponse.ok("주문 상태가 변경되었습니다.", responseDto));
 	}
 }
